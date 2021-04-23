fc2209a3c9c79745b02a974dbbec82dc4b8ff509
thiagoluiz <thiagoluiz.work@gmail.com>
2021-04-23 20:39:06 -0300

    Estrutura do Backend Java

---
+++ Backend/projetoFinalApi/src/main/java/br/com/cotiinformatica/ProjetoFinalApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalApiApplication.class, args);
+	}
+
+}
